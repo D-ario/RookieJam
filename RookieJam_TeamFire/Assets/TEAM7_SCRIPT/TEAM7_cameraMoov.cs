1d6828bdbdd41d6aef7f6fb96491c332c887ba83
Bifflou <gut.enzo02122001@gmail.com>
2020-11-04 10:50:45 +0100

    Rajout de mon travail
    script + sound + cam + prefabs

---
+++ RookieJam_TeamFire/Assets/TEAM7_SCRIPT/TEAM7_cameraMoov.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEAM7_cameraMoov : MonoBehaviour
+{
+    public int mouvValue = 1;
+    public bool isDebited = false;
+
+    private bool actif = true;
+
+
+    void Update() {
+        if ((Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D) ||
+             Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.RightArrow)) 
+             && actif == true)
+        {                 
+            StartCoroutine(moovLess());
+            isDebited = false;
+            
+        }
+    }
+
+    IEnumerator moovLess()
+    {
+        actif = false;
+        TEAM7_mouvement.instance.ChangeScore(mouvValue, isDebited); 
+        yield return new WaitForSeconds(1);
+        actif = true;
+    }
+}
---
+++ RookieJam_TeamFire/Assets/TEAM7_SCRIPT/TEAM7_score.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TEAM7_score : MonoBehaviour
+{
+    public static TEAM7_score instance;
+    public Text text;
+    int team7_score = 100;
+
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        text.text = "Points: 100";
+    }
+
+    public void ChangeScore(int scoreValue, bool isAugmented)
+    {
+        if (isAugmented == true)
+        {
+            team7_score += scoreValue;
+        }
+        if (isAugmented == false)
+        {
+            team7_score -= scoreValue;
+        }
+        
+        text.text = "Points: " + team7_score.ToString();
+    }
+}
